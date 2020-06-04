e26f4afa2326b82365f489d0146203ad1aa32c6b
Jorge Ramirez <joramirez96@gmail.com>
2020-06-04 23:28:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,14 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  
-  if phrase.downcase
+
+  if phrase != words.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  end
-  if phrase.upcase
+
+  elsif words == 'I LOVE YOU GRANDMA!'
     return "I LOVE YOU TOO PUMPKIN!"
+
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
